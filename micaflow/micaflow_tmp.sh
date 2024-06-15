49ec8ef00e7516077251fa0bc7ff7a4341f4234a
cerysojj <cerysjenkins2@gmail.com>
2024-06-15 10:47:36 -0400

    Jaccard Index calculations

--- micaflow/micaflow_tmp.sh
+++ micaflow/micaflow_tmp.sh
@@ -50,6 +50,9 @@ while [[ $# -gt 0 ]]; do
       ;;
     --threads)
       threads="$2"
+      if [ -z "${threads}" ]; then
+        threads=6
+      fi
       echo "Input --threads : ${threads}"
       shift
       ;;
@@ -69,12 +72,26 @@ done
 micaflow_dir=$(dirname $(realpath "${0}"))
 
 # ------------------------------------------------- Unit test -------------------------------------------------- #
-# Error exit if fs_license is not provided (not now, but at the end)
-# Set a default of threads if the user did not provide them. 6
-# Do this for mandatory inputs
-# Optional inputs
-# Check if the provided subjec/session exists: ${bids}/${subject}/${session} if not error and exit
-# If the outputs exist WARNING and overwrite or ERROR and exit
+if [ -z "$fs_license" ]; then
+  echo "Error: --fs_license is required."
+  exit 1
+fi
+
+if [ -z "$subject" ] || [ -z "$session" ] || [ -z "$bids" ]; then
+  echo "Error: --subject, --session, and --bids directories are required."
+  exit 1
+fi
+
+# Check if the subject/session directory exists
+if [ ! -d "${bids}/${subject}/${session}" ]; then
+  echo "Error: Directory ${bids}/${subject}/${session} does not exist."
+  exit 1
+fi
+
+# Check for existing output directory and handle accordingly
+if [ -d "${out_dir}/${subject}/${session}" ]; then
+  echo "Warning: Output directory already exists. Contents may be overwritten."
+fi
 
 # ------------------------------------------------- Initiate timer -------------------------------------------------- #
 SECONDS=0
@@ -146,97 +163,15 @@ function micaflow(){
 micaflow ${T1w} "T1w"
 micaflow ${flair} "FLAIR"
 
-# # -------------------------------------------- Step 1. Brain segmentation ------------------------------------------- #
-# echo -e "\nStep 1: Brain segmentation\n"
-#
-# # Segmentation of the T1w image
-# # Take into account including or not the option --cpu (optional) Enforce running with CPU rather than GPU.
-# mri_synthseg --i ${T1w} \
-#              --o ${out}/anat/${BIDS_ID}_desc-synthseg_T1w.nii.gz \
-#              --robust --vol ${out}/anat/${BIDS_ID}_desc-volumes_T1w.csv \
-#              --qc ${out}/anat/${BIDS_ID}_desc-qc_T1w.csv --threads ${threads} --cpu --parc
-#
-# # Segmentation of the FLAIR image
-# mri_synthseg --i ${flair} \
-#             --o ${out}/anat/${BIDS_ID}_desc-synthseg_FLAIR.nii.gz \
-#             --robust --vol ${out}/anat/${BIDS_ID}_desc-volumes_FLAIR.csv \
-#             --qc ${out}/anat/${BIDS_ID}_desc-qc_FLAIR.csv --threads ${threads} --cpu --parc
-#
-# # ------------------------------------------- Step 2. Bias field correction ----------------------------------------- #
-# echo -e "\nStep 2. Bias field correction\n"
-# # N4 multi-thread requires this on the ENV
-# export ITK_GET_GLOBAL_DEFAULT_NUMBER_OF_THREADS=${threads}
-#
-# # Bias field correction (ANTS) for T1w
-# N4BiasFieldCorrection  -d 3 -i "$T1w" -r -o "${tmp_dir}/${BIDS_ID}_desc-N4_T1w.nii.gz" -v
-#
-# # Bias field correction for FLAIR
-# N4BiasFieldCorrection  -d 3 -i "$flair" -r -o "${tmp_dir}/${BIDS_ID}_desc-N4_FLAIR.nii.gz" -v
-#
-# # -------------------------------------- Step 3. Registration of T1w to MNI space ----------------------------------- #
-# echo -e "\nStep 3. Registration of T1w to MNI space\n"
-#
-# # Output of the segmentation, this and the nifti must be inside the repository!
-# atlas_dir=/host/yeatman/local_raid/cerys/Downloads/mni_icbm152_nlin_sym_09a
-#
-# # MNI152 compresed NIFTI
-# atlas_mni152=${atlas_dir}/mni_icbm152_t1_tal_nlin_sym_09a.nii.gz
-# atlas_mni152_seg=${atlas_dir}/mni_icbm152_t1_tal_nlin_sym_09a_synthseg.nii.gz
-#
-# # Registration from T1w to MNI
-# mri_easyreg --ref ${atlas_mni152} \
-#             --ref_seg ${atlas_mni152_seg} \
-#             --flo ${T1w} \
-#             --flo_seg ${out}/anat/${BIDS_ID}_desc-synthseg_T1w.nii.gz \
-#             --fwd_field ${out}/xfm/${BIDS_ID}_from-T1w_to-MNI151_desc-easyreg_fwdfield.nii.gz \
-#             --bak_field ${out}/xfm/${BIDS_ID}_from-T1w_to-MNI151_desc-easyreg_bakfield.nii.gz \
-#             --threads ${threads}
-#
-# # --------------------------------------------- Step 4. Apply the warpfield ----------------------------------------- #
-# echo -e "\nStep 4. Apply spatial normalization warpfield\n"
-#
-# # Apply the warpfield
-# mri_easywarp --i ${tmp_dir}/${BIDS_ID}_desc-N4_T1w.nii.gz \
-#              --o ${out}/anat/${BIDS_ID}_space-MNI152_T1w.nii.gz \
-#              --field ${out}/xfm/${BIDS_ID}_from-T1w_to-MNI151_desc-easyreg_fwdfield.nii.gz \
-#              --threads ${threads} --nearest
-#
-# # ------------------------------------------- Step 5. Align T2/FLAIR to T1w ----------------------------------------- #
-# echo -e "\nStep 5. Align T2/FLAIR to T1w\n"
-#
-# # Register FLAIR to T1w
-# mri_easyreg --ref ${tmp_dir}/${BIDS_ID}_desc-N4_T1w.nii.gz \
-#             --ref_seg ${out}/anat/${BIDS_ID}_desc-synthseg_T1w.nii.gz \
-#             --flo ${flair} \
-#             --flo_seg ${out}/anat/${BIDS_ID}_desc-synthseg_FLAIR.nii.gz \
-#             --fwd_field ${out}/xfm/${BIDS_ID}_from-FLAIR_to-T1w_desc-easyreg_fwdfield.nii.gz \
-#             --bak_field ${out}/xfm/${BIDS_ID}_from-FLAIR_to-T1w_desc-easyreg_bakfield.nii.gz \
-#             --threads ${threads}
-#
-# # --------------------------------------- Step 6. Apply warpfied of T1w to FLAIR ------------------------------------- #
-# echo -e "\nStep 6. Apply forward transformation to FLAIR\n"
-#
-# # Apply forward transformation of flair to T1w space
-# mri_easywarp --i ${tmp_dir}/${BIDS_ID}_desc-N4_FLAIR.nii.gz \
-#              --o ${out}/anat/${BIDS_ID}_space-T1w_FLAIR.nii.gz \
-#              --field ${out}/xfm/${BIDS_ID}_from-FLAIR_to-T1w_desc-easyreg_fwdfield.nii.gz \
-#              --threads ${threads} --nearest
-#
-# # Apply forward transformation of flair in the T1w space to MNI space
-# mri_easywarp --i ${out}/anat/${BIDS_ID}_space-T1w_FLAIR.nii.gz  \
-#              --o ${out}/anat/${BIDS_ID}_space-MNI152_FLAIR.nii.gz \
-#              --field ${out}/xfm/${BIDS_ID}_from-T1w_to-MNI151_desc-easyreg_fwdfield.nii.gz \
-#              --threads ${threads} --nearest
-
-# ----------------------------------- Step 7. Calculate Registration Quality Metrics -------------------------------- #
+# ----------------------------------- Step 5. Calculate Registration Quality Metrics -------------------------------- #
+mkdir -p ${out}/scores
 # Compute Dice score and Jaccard Index of T1 registration.
-python "${micaflow_dir}"/calculate_metrics.py "${out}/anat/${BIDS_ID}_space-MNI152_T1w.nii.gz" "${atlas_mni152_seg}"
+python "${micaflow_dir}"/calculate_metrics.py "${out}/anat/${BIDS_ID}_space-MNI152_T1w.nii.gz" "${atlas_mni152_seg}" "{out}/scores/${BIDS_ID}_T1w.csv"
 
 # Compute Dice score and Jaccard Index of T2 registration.
-python "${micaflow_dir}"/calculate_metrics.py "${out}/anat/${BIDS_ID}_space-MNI152_FLAIR.nii.gz" "${atlas_mni152_seg}"
+python "${micaflow_dir}"/calculate_metrics.py "${out}/anat/${BIDS_ID}_space-MNI152_FLAIR.nii.gz" "${atlas_mni152_seg}" "{out}/scores/${BIDS_ID}_FLAIR.csv"
 
-# ----------------------------------- Step 8. Normalize images -------------------------------- #
-
+# ----------------------------------- Step 6. Normalize images? -------------------------------- #
 # remove tmp directory
 rm -rfv "${tmp_dir}"
 
