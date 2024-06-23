37db83e3893ac481749aabce4889f297e984c2ce
cerysojj <cerysjenkins2@gmail.com>
2024-06-23 12:24:23 -0400

    added optional argument of ROI mask

--- micaflow/calculate_metrics.py
+++ micaflow/calculate_metrics.py
@@ -1,14 +1,32 @@
 import sys
 import csv
 from nipype.algorithms.metrics import Overlap
+import nibabel as nib
 
-def main(image, reference, output_file):
+def apply_threshold(image_path, threshold=0.5):
+    img = nib.load(image_path)
+    data = img.get_fdata()
+    data[data < threshold] = 0
+    nib.save(nib.Nifti1Image(data, img.affine), image_path)
+
+def main(image, reference, output_file, threshold=0.5, mask_path=None):
+
+    # Apply threshold to remove small regions
+    apply_threshold(image, threshold)
+    apply_threshold(reference, threshold)
     
     overlap = Overlap()
     overlap.inputs.volume1 = image
     overlap.inputs.volume2 = reference
-    #overlap.inputs.bg_overlap = True (Consider zeros as a label)
+
+    if mask_path:
+        overlap.inputs.mask_volume = mask_path
+
     res = overlap.run()
+
+    # Print the number of ROIs
+    num_rois = len(res.outputs.roi_ji)
+    print("Number of ROIs:", num_rois)
 
     with open(output_file, 'w', newline='') as file:
         csvwriter = csv.writer(file)
@@ -17,11 +35,13 @@ def main(image, reference, output_file):
             csvwriter.writerow([i + 1, ji]) 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    if len(sys.argv) < 4 or len(sys.argv) > 5:
+        print("Usage: python calculate_metrics.py <volume1> <volume2> <output_csv> [<mask_path>]")
+        sys.exit(1)
+    
+    volume1 = sys.argv[1]
+    volume2 = sys.argv[2]
+    output_csv = sys.argv[3]
+    mask_path = sys.argv[4] if len(sys.argv) == 5 else None
 
-'''
-python3 calculate_metrics.py \
-/Users/cerys/Documents/MICA/test_data/test_outputs/sub-HC062/sub-HC062_ses-03_space-MNI152_T1w.nii.gz \
-/Users/cerys/Documents/MICA/micaflow/micaflow/atlas/mni_icbm152_t1_tal_nlin_sym_09a.nii.gz \
-/Users/cerys/Documents/MICA/test_data/test_outputs/sub-HC062/ses-03/sub-HC062_ses-03_jaccard_indices.csv
-'''
+    main(volume1, volume2, output_csv, mask_path=mask_path)
